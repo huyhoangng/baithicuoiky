38f1cc099e75bf6a92a7022cb95bafd3622e5d54
huyhoangng <133932405+huyhoangng@users.noreply.github.com>
2023-06-01 09:20:58 +0700

    Add files via upload

--- ex1.c
+++ ex1.c
@@ -2,6 +2,7 @@
 // Created by ADM on 6/1/2023.
 //
 #include <stdio.h>
+int isEvenNumber(int num);
 void main(){
     int num1,num2;
     int num;
@@ -12,11 +13,15 @@ void main(){
     printf("\n+num2:");
     scanf("%d",&num2);
     for (num=num1;num<num2;num++){
-    sum=num%2;
-    if (sum==0) {
-        printf("\nThe even number between %d and %d is\n", num1, num2);
-        printf("%d", num);
-    }
+        sum=isEvenNumber(num);
+        if(sum==0)
+            printf("The even number between %d and %d is %d\n",num1,num2,num);
+
     }
 
+}
+int isEvenNumber(int num){
+    int kq;
+    kq=num%2;
+    return (kq);
 }
